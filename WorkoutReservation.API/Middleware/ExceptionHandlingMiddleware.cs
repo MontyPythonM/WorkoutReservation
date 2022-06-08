245cc7e208dfa0b3bfe1bc3fc22b77a153c39216
Mateusz Szewczyk <mat.szewczyk96@gmail.com>
2022-06-08 12:04:58 +0200

    Add exception handling middlerawe with custom exceptions (NotFound, Validation, BadRequest)

---
+++ WorkoutReservation.API/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using WorkoutReservation.Application.Exceptions;
+
+namespace WorkoutReservation.API.Middleware
+{
+    public class ExceptionHandlingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (ValidationException ex)
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync(ex.Message);
+            }
+            catch (NotFoundException ex)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                await context.Response.WriteAsync(ex.Message);
+            }
+            catch (BadRequestException ex)             
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                await context.Response.WriteAsync(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync(ex.Message);
+            }
+        }
+    }
+}
---
+++ WorkoutReservation.Application/Common/Exceptions/ValidationException.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkoutReservation.Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException(string message) : base(message)
+        {
+        }
+    }
+}
