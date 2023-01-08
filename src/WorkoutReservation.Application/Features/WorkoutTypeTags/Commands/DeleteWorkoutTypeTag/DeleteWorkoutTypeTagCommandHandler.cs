97c7979cd253b380eebd596b4e4bebc30262b2ee
Mateusz Szewczyk <mat.szewczyk96@gmail.com>
2023-01-08 22:13:48 +0100

    Add delete workout type tag endpoint

---
+++ src/WorkoutReservation.Application/Features/WorkoutTypeTags/Commands/DeleteWorkoutTypeTag/DeleteWorkoutTypeTagCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using WorkoutReservation.Application.Common.Exceptions;
+using WorkoutReservation.Application.Contracts;
+
+namespace WorkoutReservation.Application.Features.WorkoutTypeTags.Commands.DeleteWorkoutTypeTag;
+
+public class DeleteWorkoutTypeTagCommandHandler : IRequestHandler<DeleteWorkoutTypeTagCommand>
+{
+    private readonly IWorkoutTypeTagRepository _workoutTypeTagRepository;
+
+    public DeleteWorkoutTypeTagCommandHandler(IWorkoutTypeTagRepository workoutTypeTagRepository)
+    {
+        _workoutTypeTagRepository = workoutTypeTagRepository;
+    }
+    
+    public async Task<Unit> Handle(DeleteWorkoutTypeTagCommand request, CancellationToken token)
+    {
+        var workoutTypeTag = await _workoutTypeTagRepository.GetByIdAsync(request.Id, false, token);
+
+        if (workoutTypeTag is null)
+            throw new NotFoundException($"Workout type with Id: {request.Id} not found.");
+
+        await _workoutTypeTagRepository.DeleteAsync(workoutTypeTag, token);
+        return Unit.Value;
+    }
+}
