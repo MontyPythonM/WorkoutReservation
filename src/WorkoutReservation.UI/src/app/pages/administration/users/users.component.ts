c8380aec9833115c67bb4a0e81612fd4a9d446f7
Mateusz Szewczyk <mat.szewczyk96@gmail.com>
2023-01-17 23:50:23 +0100

    Add pager to users data grid

--- src/WorkoutReservation.UI/src/app/pages/administration/users/users.component.ts
+++ src/WorkoutReservation.UI/src/app/pages/administration/users/users.component.ts
@@ -4,6 +4,8 @@ import {UserService} from "../../../services/user.service";
 import {PagedResult} from "../../../models/paged-result.model";
 import {User} from "../../../models/user.model";
 import {PagedQuery} from "../../../models/paged-query.model";
+import DevExpress from "devextreme";
+import dxDataGrid = DevExpress.ui.dxDataGrid;
 
 @Component({
   selector: 'app-users',
@@ -13,12 +15,21 @@ import {PagedQuery} from "../../../models/paged-query.model";
 export class UsersComponent extends BaseComponent implements OnInit {
 
   users?: PagedResult<User>;
-  query: PagedQuery;
+  pagerInfo: string;
+  private grid: any;
+  private query: PagedQuery;
 
   constructor(private userService: UserService) {
     super();
     this.users = new PagedResult<User>();
-    this.query = PagedQuery.default();
+    this.query = new PagedQuery({
+      pageNumber: 1,
+      pageSize: 15,
+      sortByDescending: false,
+      sortBy: '',
+      searchPhrase: ''
+    });
+    this.pagerInfo = "";
   }
 
   ngOnInit(): void {
@@ -27,7 +38,24 @@ export class UsersComponent extends BaseComponent implements OnInit {
 
   loadUsers(query: PagedQuery) {
     this.subscribe(this.userService.getUsers(query), {
-      next: (response: PagedResult<User>) => this.users = response
+      next: (response: PagedResult<User>) => {
+        this.users = response;
+        this.pagerInfo = `Total items: ${ this.users?.totalItemsCount } | Items from ${ this.users?.itemsFrom } to ${ this.users?.itemsTo }`;
+      }
     });
   }
+
+  propertyChange(e: any) {
+    if(e.fullName === "paging.pageSize") {
+      this.query.pageSize = e.value
+      this.loadUsers(this.query);
+    }
+
+    if(e.fullName === "paging.pageIndex") {
+      this.query.pageNumber = e.value
+      this.loadUsers(this.query);
+    }
+  }
+
+  onGridInitialized = (e: {component: dxDataGrid}) => this.grid = e.component;
 }
