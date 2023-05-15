a79c919e481741d97092acc5e15fb936ecf93769
Mateusz Szewczyk <mat.szewczyk96@gmail.com>
2023-05-15 21:23:57 +0200

    Add reservation cancelled event

---
+++ src/WorkoutReservation.Application/Features/RealWorkouts/Events/ReservationCancelledEventHandler.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using WorkoutReservation.Application.Contracts;
+using WorkoutReservation.Application.Exceptions;
+using WorkoutReservation.Domain.Entities;
+using WorkoutReservation.Domain.Events;
+using WorkoutReservation.Domain.Exceptions;
+
+namespace WorkoutReservation.Application.Features.RealWorkouts.Events;
+
+public class ReservationCancelledEventHandler : INotificationHandler<ReservationCancelledEvent>
+{
+    private readonly IEmailSender _emailSender;
+    private readonly IEmailBuilder _emailBuilder;
+    private readonly IRealWorkoutRepository _realWorkoutRepository;
+
+    public ReservationCancelledEventHandler(IEmailSender emailSender, IEmailBuilder emailBuilder, 
+        IRealWorkoutRepository realWorkoutRepository)
+    {
+        _emailSender = emailSender;
+        _emailBuilder = emailBuilder;
+        _realWorkoutRepository = realWorkoutRepository;
+    }
+    
+    public async Task Handle(ReservationCancelledEvent @event, CancellationToken token)
+    {
+        var realWorkout = await _realWorkoutRepository
+            .GetByReservationIdAsync(@event.ReservationId, true, token);
+
+        if (realWorkout is null)
+            throw new NotFoundException(nameof(RealWorkout), @event.ReservationId.ToString());
+        
+        var reservation = realWorkout.Reservations
+            .Single(reservation => reservation.Id == @event.ReservationId);
+
+        var instructorFullName = string.Join(" ", realWorkout.Instructor.FirstName, realWorkout.Instructor.LastName);
+        
+        var message = _emailBuilder.CreateSendGridMessage(reservation.User.Email, 
+            $"Your reservation with ID: {@event.ReservationId} on {realWorkout.WorkoutType.Name} with {instructorFullName} was cancelled.", 
+            $"Reservation cancelled - {realWorkout.WorkoutType.Name}");
+        
+        await _emailSender.SendEmail(message, token);
+    }
+}
